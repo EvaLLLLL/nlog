24b95e8e1fb9d4f2b16523abcaac692353f6f09f
lyhhh <17610603221@163.com>
2021-07-17 15:53:12 +0800

    first-post

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,12 @@
+/**
+ * @file FIX ME WHEN YOU SEE ME! 请对本文件的用途或内容进行说明...
+ */
+ 
+ import React from 'react'
+ 
+const FirstPost: React.FC = () => {
+    
+    return (<>FirstPost</>)
+ }
+
+ export default FirstPost
