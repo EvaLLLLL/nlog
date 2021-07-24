e4eccb62829bf9383d107f02a44d8c1ec305a503
lyhhh <17610603221@163.com>
2021-07-25 02:06:24 +0800

    seed 构造数据

--- src/seed.ts
+++ src/seed.ts
@@ -5,13 +5,15 @@ import { Post } from './entity/Post';
 createConnection()
   .then(async connection => {
     const posts = await connection.manager.find(Post);
-    console.log(posts);
-    const p = new Post();
-    p.title = 'Post 1';
-    p.content = '我的第一篇文章';
-    await connection.manager.save(p);
-    const post2 = await connection.manager.find(Post);
-    console.log(post2);
+
+    if (!posts.length) {
+      await connection.manager.save([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(n => {
+        return new Post({title: `Post ${n}`, content: `这是我的第 ${n} 篇文章`})
+      }))
+
+      console.log('posts 数据填充完毕')
+    }
+
     await connection.close();
   })
   .catch(error => console.log(error));
