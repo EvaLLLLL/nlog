94c86bc15f4aaa38a34f1fcfe1eecc2a37a758ce
lyhhh <17610603221@163.com>
2021-07-25 18:17:14 +0800

    修复timestamp字段

--- src/seed.ts
+++ src/seed.ts
@@ -1,8 +1,18 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import { User } from './entity/User';
 
 createConnection()
   .then(async connection => {
+    const { manager } = connection;
+
+    const user1 = new User();
+    user1.username = 'eva';
+    user1.passwordDigest = '123';
+    await manager.save(user1);
+
+    console.log(user1);
+
     await connection.close();
   })
   .catch(error => console.log(error));
